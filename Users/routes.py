d1101e0cf6bf93c36b8b7158bf632f9a29a1267c
AyoubMS <chahayb563@gmail.com>
2023-07-16 20:48:20 +0200

    add initial setup

--- Users/routes.py
+++ Users/routes.py
@@ -7,36 +7,3 @@ from flask_login import login_user, login_required, logout_user
 routes = Blueprint('routes', __name__)
 
 # Your Code Here
-@routes.route('/signup', methods=['POST'])
-def signup():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    name = request.form.get('name')
-    user = User.query.filter_by(email=email).first()
-    if user:
-        return None
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-    db.session.add(new_user)
-    db.session.commit()
-    response = jsonify(new_user.toDict())
-    response.status_code = 200
-    return response
-
-@routes.route('/login', methods=['GET', 'POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-    user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
-        return None
-    login_user(user, remember=remember)
-    result = jsonify(user.toDict())
-    result.status_code = 200
-    return result
-
-@routes.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return None
